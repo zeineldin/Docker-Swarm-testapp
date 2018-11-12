52a652dfbe02c4ea16c31cd92338135301909da2
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2018-11-12 15:14:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 //whether ip is from share internet
-$instance_id = shell_exec('hostname');
+$container_id = shell_exec('hostname');
 echo "<br>";
 echo "<br>";
-echo " Container Id = $instance_id";
+echo " Container Id = $container_id";
 echo "<br>";
 echo "<br>";
 ?>
